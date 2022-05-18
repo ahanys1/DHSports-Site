957198852a711872cf6cb79fdf313cdcaff474cc
ahanys1 <alexanderhanys@gmail.com>
2022-05-17 23:49:05 -0400

    beginings

---
+++ app.js
@@ -0,0 +1,27 @@
+// imports
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//static files
+app.use(express.static('public'));
+app.use("/css", express.static(__dirname + '/public/css'));
+app.use("/js", express.static(__dirname + '/public/js'));
+app.use("/img", express.static(__dirname + '/public/img'));
+
+//set views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+//nav
+app.get('', (req,res) => {
+    res.render('index',{text: 'this is variable'});
+})
+app.get('/about', (req,res) => {
+    res.render('about',{text: 'about page'});
+})
+
+
+//listen on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
